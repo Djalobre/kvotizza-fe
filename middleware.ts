c7d319231216aa6302861149724f9d2648b9b38a
Djalo <52053245+Djalobre@users.noreply.github.com>
2025-08-25 01:12:47 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -16,9 +16,6 @@ export default async function middleware(req: NextRequest) {
 
   // not signed in
   if (!token) {
-    if (req.nextUrl.pathname.startsWith("/api")) {
-      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-    }
     const signInUrl = new URL("/api/auth/signin", req.url);
     signInUrl.searchParams.set("callbackUrl", req.nextUrl.pathname + req.nextUrl.search);
     return NextResponse.redirect(signInUrl);
