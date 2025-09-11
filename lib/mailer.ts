4e77498da1f14963367b6952439c65d28bae411d
Djalo <52053245+Djalobre@users.noreply.github.com>
2025-09-11 13:12:23 +0200

    Update mailer.ts

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,10 +1,19 @@
 // lib/mailer.ts
-import { Resend } from 'resend'
-const resend = new Resend(process.env.RESEND_API_KEY!)
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+})
 
 export async function sendMail(to: string, subject: string, html: string) {
-  await resend.emails.send({
-    from: process.env.MAIL_FROM || 'auth@yourdomain.com',
-    to, subject, html,
+  await transporter.sendMail({
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html,
   })
 }
