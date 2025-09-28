956f47986ade25ba232784fe587a17c9b63b40a7
Djalo <52053245+Djalobre@users.noreply.github.com>
2025-09-28 19:45:33 +0200

    Update analitika.tsx

--- app/analitika/analitika.tsx
+++ app/analitika/analitika.tsx
@@ -1,22 +1,28 @@
 // app/analytics.tsx
-"use client";
+'use client';
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import * as gtag from "@/lib/gtag";
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect, Suspense } from 'react';
+import * as gtag from '@/lib/gtag';
 
-export default function Analytics() {
+function AnalyticsComponent() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
     if (pathname) {
-      const url =
-        pathname +
-        (searchParams.toString() ? `?${searchParams.toString()}` : "");
+      const url = pathname + (searchParams.toString() ? `?${searchParams.toString()}` : '');
       gtag.pageview(url);
     }
   }, [pathname, searchParams]);
 
   return null;
+}
+
+export default function Analytics() {
+  return (
+    <Suspense fallback={null}>
+      <AnalyticsComponent />
+    </Suspense>
+  );
 }
